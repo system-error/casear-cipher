5cb035b71bc0eed5441c3b6b729a288b1feb917e
system-error <christos_dousmanopoulos@yahoo.gr>
2018-09-15 21:10:23 +0300

    First

---
+++ cipher.py
@@ -0,0 +1,33 @@
+key = 1
+text = 'Hello World'
+encrypted_text = ""
+table = []
+print(ord(' '))
+for char in text:
+    if char.isalpha():
+        if char.islower():
+            chaar = ord(char)
+            aplphabet = ((ord(char) - ord('a') + key) % 26) + ord('a')
+            # if aplphabet > ord('z'):
+            #     aplphabet -= 26
+            newletter = chr(aplphabet)
+            encrypted_text += "".join(newletter)
+        else:
+            chaar = ord(char)
+            aplphabet = ((ord(char) - ord('A') + key) % 26) + ord('A')
+            # if aplphabet > ord('z'):
+            #     aplphabet -= 26
+            newletter = chr(aplphabet)
+            encrypted_text += "".join(newletter)            
+    elif char == ' ':    
+       encrypted_text += "".join(char)
+    # table.append(newletter)
+    
+
+print(encrypted_text)
+
+
+
+# new_position = ((ord(letter) - ord('a') + key) % 26) + ord('a')
+#         new_ch = chr(new_position)
+#         ciphertext += new_ch

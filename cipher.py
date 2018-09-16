e322db27c5377fef7242ce3eee0ea4a47e6b67c2
system-error <christos_dousmanopoulos@yahoo.gr>
2018-09-16 13:38:05 +0300

    First

--- cipher.py
+++ cipher.py
@@ -3,22 +3,15 @@ def encryption(text,key=0):
     for char in text:
         if char.isalpha():
             if char.islower():
-                chaar = ord(char)
                 aplphabet = ((ord(char) - ord('a') - key) % 26) + ord('a')
-            # if aplphabet > ord('z'):
-            #     aplphabet -= 26
                 newletter = chr(aplphabet)
                 encrypted_text += "".join(newletter)
             else:
-                chaar = ord(char)
                 aplphabet = ((ord(char) - ord('A') - key) % 26) + ord('A')
-            # if aplphabet > ord('z'):
-            #     aplphabet -= 26
                 newletter = chr(aplphabet)
                 encrypted_text += "".join(newletter)            
         elif char == ' ':    
             encrypted_text += "".join(char)
-    # table.append(newletter)
     return encrypted_text
 
 def decryption(text,key=0):
@@ -26,22 +19,15 @@ def decryption(text,key=0):
     for char in text:
         if char.isalpha():
             if char.islower():
-                chaar = ord(char)
                 aplphabet = ((ord(char) - ord('a') + key) % 26) + ord('a')
-            # if aplphabet > ord('z'):
-            #     aplphabet -= 26
                 newletter = chr(aplphabet)
                 decrypted_text += "".join(newletter)
             else:
-                chaar = ord(char)
                 aplphabet = ((ord(char) - ord('A') + key) % 26) + ord('A')
-            # if aplphabet > ord('z'):
-            #     aplphabet -= 26
                 newletter = chr(aplphabet)
                 decrypted_text += "".join(newletter)            
         elif char == ' ':    
             decrypted_text += "".join(char)
-    # table.append(newletter)
     return decrypted_text
 
 key = 1
